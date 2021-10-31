536b58285be69b960791840a9783db7167948599
Alessio Sanfratello <sanfra90@gmail.com>
2021-10-31 12:12:05 +0100

    Added canonical base 10 conversion

--- Dozen WatchKit Extension/ContentView.swift
+++ Dozen WatchKit Extension/ContentView.swift
@@ -8,22 +8,32 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State var currentDate = ""
+    @State var currentDate12 = "Dozen"
+    @State var currentDate10 = "The best clock"
     
     let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
     let calendar = Calendar.current
-
+    
     var body: some View {
-        Text("\(currentDate)")
-            .bold()
-            .font(.system(size: 36))
-            .onReceive(timer) { input in
-                let hour = calendar.component(.hour, from: input)
-                let minute = calendar.component(.minute, from: input)
-                let second = calendar.component(.second, from: input)
-                
-                currentDate = "\(hour/12)\(String(format: "%01X", hour%12)):\(minute/12)\(String(format: "%01X", minute%12)):\(second/12)\(String(format: "%01X", second%12))"
-            }
+        VStack {
+            Text("\(currentDate12)")
+                .font(.system(size: 36, weight: .bold))
+                .onReceive(timer) { input in
+                    let hour = calendar.component(.hour, from: input)
+                    let minute = calendar.component(.minute, from: input)
+                    let second = calendar.component(.second, from: input)
+                    
+                    currentDate12 = "\(hour/12)\(String(format: "%01X", hour%12)):\(minute/12)\(String(format: "%01X", minute%12)):\(second/12)\(String(format: "%01X", second%12))"
+                }
+            Text("\(currentDate10)")
+                .onReceive(timer) { input in
+                    let hour = calendar.component(.hour, from: input)
+                    let minute = calendar.component(.minute, from: input)
+                    let second = calendar.component(.second, from: input)
+                    
+                    currentDate10 = "\(String(format: "%02D", hour)):\(String(format: "%02D", minute)):\(String(format: "%02D", second))"
+                }
+        }
     }
 }
 
--- README.md
+++ README.md
@@ -3,3 +3,7 @@
 A simple Apple Watch app to show time in the best format, base 12!
 
 E.g. 18:35 -> 16:2A
+
+## Preview
+
+![Applicaion sceenshot](/assets/preview.png?raw=true "Dozen")
